7e57f4d57da18bf8574bd57eb0d1fd6b43fa6740
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-27 15:22:00 +0300

    function revision

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 // //==========================================================================================================================================
 
 // //object oriented programming
-// // we can create objects using constructor functions withn prototypr.
-// //when creating a constructor functionmust start withna capital.
+// // we can create objects using constructor functions withn prototype.
+// //when creating a constructor function must start withn a capital.
 // //in constructor function to set a value of parameter, we use this keyword
 
 // //constructor function
@@ -97,37 +97,37 @@
 // video.play();
 //==========================================================================================================================================
 //function declaration
-function greet() {
-    // console.log('hello there')
-}
-greet()
-    //FUNCTION EXPRESSION
-    //place semi-colon at the end of function
-    //hoisting is effectively on function declation but it doesn't work with function expression and most prefferebly use function expression
-    // const speak = function(name, age) {
-    //     console.log(`good day ${name} ${age}`);
-    // };
-    // speak('lincolin', 33)
-    //=================================================================================================================================
-    //callback --->we can pass in a function as an arguemnt
-    // const myFunc = (myName) => {
-    //     //do something
-    //     let value = 50;
-    //     myName(value);
-    // }
-    // myFunc(function(value) {
-    //     //do somethin
-    //     console.log(value);
-    // });
-    // const calcArea = (Area) => {
-    //     let length = 50;
-    //     let width = 60;
-    //     let areaCovered = length * width;
-    //     Area(areaCovered)
-    // }
-    // calcArea((areaCovered)=> {
-    //     console.log(areaCovered)
-    // })
+// function greet() {
+//     // console.log('hello there')
+// }
+// greet()
+//FUNCTION EXPRESSION
+//place semi-colon at the end of function
+//hoisting is effectively on function declation but it doesn't work with function expression and most prefferebly use function expression
+// const speak = function(name, age) {
+//     console.log(`good day ${name} ${age}`);
+// };
+// speak('lincolin', 33)
+//=================================================================================================================================
+//callback --->we can pass in a function as an arguemnt
+// const myFunc = (myName) => {
+//     //do something
+//     let value = 50;
+//     myName(value);
+// }
+// myFunc(function(value) {
+//     //do somethin
+//     console.log(value);
+// });
+// const calcArea = (Area) => {
+//     let length = 50;
+//     let width = 60;
+//     let areaCovered = length * width;
+//     Area(areaCovered)
+// }
+// calcArea((areaCovered)=> {
+//     console.log(areaCovered)
+// })
 
 //buit in array method called foreach---->iterate over an array elements
 
@@ -140,14 +140,73 @@ greet()
 // people.forEach(function(index, person) {
 //     console.log(`${index}- hello - ${person}`)
 // })
-let ul = document.querySelector('.people')
-let people = ['mario', 'luigi', 'ryan', 'shaun', 'chun-li'];
-let html = ``
-people.forEach((person) => {
+// let ul = document.querySelector('.people')
+// let people = ['mario', 'luigi', 'ryan', 'shaun', 'chun-li'];
+// let html = ``
+// people.forEach((person) => {
 
-        //create html template
-        html += `<li style = "color:purple"> ${person} </li>`
+//         //create html template
+//         html += `<li style = "color:purple"> ${person} </li>`
 
-    })
-    // console.log(html)
-ul.innerHTML = html
+//     })
+//     // console.log(html)
+// ul.innerHTML = html
+//==========================================================================================================
+//what's in it for you
+// what are javascript functions
+//-->is a group of reusable codes which can be called anywhere in your program
+//eliminates the need to writing the same code agai and again
+//helps programmer write a modular codes
+//function function_name(parameter_list){
+// function_body
+// }
+// function my_function(val) {
+//     document.getElementById('demo').innerHTML = val * val
+// }
+// my_function(15)
+//     // function nexpression
+// var x = function(a, b) {
+//     return a * b
+// }
+// console.log(x(10, 20))
+//function()constructor
+//functions can also be defined using the built-in javascript function constructor called Function
+// var myfunction = new Function('a', 'b', 'c', 'return a+b+c')
+// var x = myfunction(2, 3, 4)
+// document.getElementById('demo').innerHTML = 'the sum is ' + x
+
+
+//self-invoking functions
+//is invoked (started) automatically if the expression is followed by ()
+// you have to add parentheses around the function to indicate that it is a function expression
+// (function() {
+
+//     function_body //i will invoke my self
+// })();
+// (function() {
+//     alert('welcome! lincoln')
+// })()
+//functions as values
+// -- - > in javascript function is a value,so we can deal with it as a value.
+//we first define the function and then assign it to a variable unlike in expression
+//with function as value, we can add opearatio to the value of the function e.g let x=product(2,4)*4
+// function function_name(parameter_list) {
+//     function_body //i will invoke myself
+// }
+// let x = function_name
+// function product(x, y) {
+//     return x * y
+// }
+// let y = product(10, 20)
+// console.log(y)
+//functions as objects
+// var body = function_body;
+// var function_name = new Function('parameter_list', body);
+// alert(function_name(arguement_value));
+// var body = "return Math.PI*rad*rad"
+// var circle = new Function("rad", body);
+// alert(circle(5.222))
+//arrow functions
+//cannot be used as constructors
+//generator functions
+//predefined functions
