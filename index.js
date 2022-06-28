ca9c15156f4772ad7b6e764c15893d9d5879a38a
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-28 12:49:03 +0300

    higher order functions

--- index.js
+++ index.js
@@ -248,36 +248,36 @@
 //a programming language is saidto havea first-class function,if functions in thatprogramming languageare treted like any other value
 //1.you can assign a functionto a variable like any other value
 //assigning a function to a variable
-let greet = function(name) {
-    return 'hello  ' + name + ' ! how are you doing?'
-}
-let msg = greet('john');
-console.log(msg);
-//2. you can pass a function as an arguement to another function
-let birthYears = [1989, 2007, 1992];
+// let greet = function(name) {
+//     return 'hello  ' + name + ' ! how are you doing?'
+// }
+// let msg = greet('john');
+// console.log(msg);
+// //2. you can pass a function as an arguement to another function
+// let birthYears = [1989, 2007, 1992];
 
-function getResultArray(arr, callback) {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        let val = callback(arr[i]);
-        result.push(val)
-    }
-}
+// function getResultArray(arr, callback) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         let val = callback(arr[i]);
+//         result.push(val)
+//     }
+// }
 
-function calcAge(birthYear) {
-    return new Date().getFullYear() - birthYear;
-}
-getResultArray(birthYears, calcAge)
-    //3.you can return a function from another function
-function interviewQuestion(job) {
-    if (job === 'teacher') {
-        let question = 'how many years of teaching experience do you have?';
-        return function(name) {
+// function calcAge(birthYear) {
+//     return new Date().getFullYear() - birthYear;
+// }
+// getResultArray(birthYears, calcAge)
+//     //3.you can return a function from another function
+// function interviewQuestion(job) {
+//     if (job === 'teacher') {
+//         let question = 'how many years of teaching experience do you have?';
+//         return function(name) {
 
-            console.log('hi ' + name + '.' + question);
-        }
-    }
-}
+//             console.log('hi ' + name + '.' + question);
+//         }
+//     }
+// }
 //HIGHER ORDER FUNCTIONS
 //are the functions which operats on otherfunctions,either by taking themas arguementor by returning them.
 //in simplewords,higher functions is a function that
@@ -285,4 +285,126 @@ function interviewQuestion(job) {
 //returns the function as output
 //it does both i.ereceives a function as argument as well as returs a function
 //-higher order functions is only possiblebecauseof the presence offirst class functions in programming languages.
-//function such as filter(),reduce(),map(),forEach() are the examplesof higherorder functions.because all these function take this another functionas its arguement
+//function such as filter(),reduce(),map(),forEach() are the examplesof higher order functions.because all these function take this another functionas its arguement
+//==========================================================================================================================================
+//CLOSURE--->functions with a preserved data
+//a function binded together with its lexical scope 
+// let addTo = function(passed) {
+//     let add = function(inner) {
+//         return passed + inner;
+//     };
+//     return add;
+// }
+// let addThree = new addTo(3)
+// let addFour = new addTo(4)
+//     // console.dir(addThree)
+// console.dir(addFour)
+// function x() {
+//     var a = 7;
+
+//     function y() {
+//         console.log(a)
+//     }
+//     y()
+// }
+// x()
+//higher array methods
+const companies = [{
+        name: 'company one',
+        category: 'Finance',
+        start: 1981,
+        end: 2003
+    },
+    {
+        name: 'company two',
+        category: 'retail',
+        start: 1982,
+        end: 2007
+    },
+
+    {
+        name: 'company three',
+        category: 'technology',
+        start: 1983,
+        end: 2009
+    },
+    {
+        name: 'company four',
+        category: 'auto',
+        start: 1981,
+        end: 2010
+    },
+    {
+        name: 'company five',
+        category: 'Finance',
+        start: 1989,
+        end: 2012
+    },
+    {
+        name: 'company six',
+        category: 'Finance',
+        start: 1999,
+        end: 2004
+    },
+    {
+        name: 'company seven',
+        category: 'retail',
+        start: 1986,
+        end: 2003
+    },
+    {
+        name: 'company eight',
+        category: 'technology',
+        start: 1984,
+        end: 2005
+    }
+];
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32]
+    //forEach
+
+// companies.forEach(function(company) {
+//     console.log(company)
+// })
+
+//filter
+// const canDrink = ages.filter(function(age) {
+//     if (age >= 21) {
+//         return true;
+//     }
+// });
+//const canDRink=ages.filter(age=>age>=21);
+// console.log(canDrink)
+//filter company category
+// const retailCompanies = companies.filter(function(company) {
+//     if (company.category === 'retail') {
+//         return true
+//     }
+// })
+
+// const retailCompany = companies.filter(company => company.category === 'retail')
+// console.log(retailCompany)
+//get 80's companies
+// const startYear = companies.filter(company => (company.start >= 1980 && company.start < 1989))
+// console.log(startYear)
+//company lastd more than ten years
+// const companyDuration = companies.filter(company => ((company.end - company.start) < 10))
+// console.log(companyDuration)
+//map----create new array from current array
+//create array of company names
+// const companyNames = companies.map(company => company.name)
+// console.log(companyNames)
+// const companyCategory = companies.map(company => company.category)
+// console.log(companyCategory)
+//sort
+//sort companie by start year
+// const sortCompany = companies.sort((c1, c2) => (c1.start > c2.start ? 1 : -1));
+// console.log(sortCompany)
+//sort ages
+// const sortAges = ages.sort((a, b) => a - b)
+// console.log(sortAges)
+//reduce--->
+// const totalAges = ages.reduce((total, age) => total + age, 0);
+// console.log(totalAges)
+//totals yearsfotr our companies
+// const totalyears = companies.reduce((total, company) => total + (company.end - company.start), 0)
+// console.log(totalyears)
